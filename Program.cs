387037fe33bd303ebe5721a3e47d73211b1307c4
namita1409 <81958036+namita1409@users.noreply.github.com>
2022-10-17 00:17:07 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using RomanNumeralKata;
+
+RomanNumeral romanNumeral= new RomanNumeral();
+
+Console.WriteLine(" Int to Roman converter");
+Console.WriteLine("pls enter your choice\n");
+Console.WriteLine("1. Roman to Int converter \n");
+Console.WriteLine("2. Int to Roman converter \n");
+int choice = Convert.ToInt32(Console.ReadLine());
+bool endApp = false;
+// Display title as the C# Roman Numeral Converter app.
+Console.WriteLine("Console Roman Numeral Converter app C#\r");
+Console.WriteLine("------------------------\n");
+
+while (!endApp)
+{
+    if (choice == 1)
+    {
+        // Declare variables and set to empty.
+        string user_input = "";
+        int result = 0;
+
+        // Ask the user to enter roman numerals.
+        Console.Write("Type a Roman Numeral, and then press Enter: ");
+        user_input = Console.ReadLine();
+
+        //  check for empty input 
+        while (user_input.Length == 0)
+        {
+            Console.Write("This is not valid input. Please enter a valid roman numeral ");
+            user_input = Console.ReadLine();
+        }
+
+        try
+        {
+            result = romanNumeral.RomanToInt(user_input.ToUpper());
+            if (result == 0)
+            {
+                Console.WriteLine("This Roman Numeral not present in my Roman dictionary");
+            }
+            else Console.WriteLine("Your Number: {0:0.##}\n", result);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Oh no! An exception occurred " + e.Message);
+        }
+
+        Console.WriteLine("------------------------\n");
+
+        // Wait for the user to respond before closing.
+        Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+        if (Console.ReadLine() == "n") endApp = true;
+
+        Console.WriteLine("\n"); // Friendly linespacing.
+    }  
+
+
+     if (choice == 2)
+    {   
+
+        // Declare variables and set to empty.
+        int input = 0;
+        string result = "";
+
+        // Ask the user to enter roman numerals.
+        Console.Write("Type a Integer number, and then press Enter: ");
+        input = Convert.ToInt32(Console.ReadLine());
+
+        try
+        {
+            result = romanNumeral.IntToRoman(input);
+
+            Console.WriteLine("Your Roman Number: {0:0.##}\n", result);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Oh no! An exception occurred " + e.Message);
+        }
+
+        Console.WriteLine("------------------------\n");
+
+        // Wait for the user to respond before closing.
+        Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+        if (Console.ReadLine() == "n") endApp = true;
+
+        Console.WriteLine("\n"); // Friendly linespacing.
+    }
+    return;
+
+}
---
+++ RomanNumeral.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumeralKata
+{
+    public class RomanNumeral
+    {
+        public string IntToRoman(int num)
+        {
+            string romanResult = "";
+            Dictionary<string, int> romanNumbersDictionary = new()
+            {
+                {
+                    "I",
+                    1
+                },
+                {
+                    "IV",
+                    4
+                },
+                {
+                    "V",
+                    5
+                },
+                {
+                    "IX",
+                    9
+                },
+                {
+                    "X",
+                    10
+                },
+                {
+                    "XL",
+                    40
+                },
+                {
+                    "L",
+                    50
+                },
+                {
+                    "XC",
+                    90
+                },
+                {
+                    "C",
+                    100
+                },
+                {
+                    "CD",
+                    400
+                },
+                {
+                    "D",
+                    500
+                },
+                {
+                    "CM",
+                    900
+                },
+                {
+                    "M",
+                    1000
+                }
+            };
+            foreach (var item in romanNumbersDictionary.Reverse())
+            {
+                if (num <= 0) break;
+                while (num >= item.Value)
+                {
+                    romanResult += item.Key;
+                    num -= item.Value;
+                }
+            }
+            return romanResult;
+        }
+        public int RomanToInt(string s)
+        {
+            int sum = 0;
+            Dictionary<char, int> romanNumbersDictionary = new()
+            {
+                { 'I', 1 },
+                { 'V', 5 },
+                { 'X', 10 },
+                { 'L', 50 },
+                { 'C', 100 },
+                { 'D', 500 },
+                { 'M', 1000 }
+            };
+            for (int i = 0; i < s.Length; i++)
+            {
+                char currentRomanChar = s[i];
+                romanNumbersDictionary.TryGetValue(currentRomanChar, out int num);
+                if (i + 1 < s.Length && romanNumbersDictionary[s[i + 1]] > romanNumbersDictionary[currentRomanChar])
+                {
+                    sum -= num;
+                }
+                else
+                {
+                    sum += num;
+                }
+            }
+            return sum;
+        }
+    }
+}
